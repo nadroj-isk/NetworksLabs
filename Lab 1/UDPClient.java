e0acc7b1e8094a56948ee6420ac1e59106e8fd66
Jordan Sosnowski <jjs002@auburn.edu>
2018-07-05 12:39:55 -0500

    Fixed Throw Error

--- Lab 1/UDPClient.java
+++ Lab 1/UDPClient.java
@@ -2,29 +2,28 @@ import java.io.*;
 import java.net.*;
 
 class UDPClient{
-  public static void main(String[] args) {
-    BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in)); //create input string
-    DatagramSocket clientSocket = new DatagramSocket(); //create client socket
-    InetAddress IPAddress = InetAddress.getByName("hostname"); //translate hostname to IP address using DNS
+  public static void main(String[] args) throws Exception {
+      BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in)); //create input string
+      DatagramSocket clientSocket = new DatagramSocket(); //create client socket
+      InetAddress IPAddress = InetAddress.getByName("hostname"); //translate hostname to IP address using DNS
 
-    byte[] sendData = new byte[1024];
-    byte[] receiveData = new byte[1024];
+      byte[] sendData = new byte[1024];
+      byte[] receiveData = new byte[1024];
 
-    String sentence = inFromUser.readLine();
-    sendData = sentence.getBytes();
+      String sentence = inFromUser.readLine();
+      sendData = sentence.getBytes();
 
-    DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, 9876);
+      DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, 9876);
 
-    clientSocket.send(sendPacket);
+      clientSocket.send(sendPacket);
 
-    DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+      DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
 
-    clientSocket.receive(receivePacket);
+      clientSocket.receive(receivePacket);
 
-    String modifiedSentence = new String(receivePacket.getData());
+      String modifiedSentence = new String(receivePacket.getData());
 
-    System.out.println("FROM SERVER" + modifiedSentence);
-    clientSocket.close();
-
+      System.out.println("FROM SERVER" + modifiedSentence);
+      clientSocket.close();
+    }
   }
-}
