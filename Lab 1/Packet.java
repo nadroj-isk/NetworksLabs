3ea5c4aa8223af148075b0a3a6e73ecee5614f30
Jordan Sosnowski <jjs002@auburn.edu>
2018-07-12 12:18:34 -0500

    Formatted Code

--- Lab 1/Packet.java
+++ Lab 1/Packet.java
@@ -1,5 +1,4 @@
-
-import java.net.*;
+import java.net.DatagramPacket;
 import java.net.InetAddress;
 import java.nio.ByteBuffer;
 import java.util.HashMap;
@@ -10,211 +9,190 @@ import java.util.Map.Entry;
 
 public class Packet {
 
-	//Constant Variables
-	private static int PackageData_SIZE = 256;
-	public static enum HEADER_ELEMENTS
-	{
-		SEGMENT_NUMBER,
-		CHECKSUM
-	}
+    //Private constant to map segment number and checksum
+    private static final String HEADER_SEGMENT_NUM = "segmentNum";
+    private static final String HEADER_CHECKSUM = "checkSum";
+    //Constant Variables
+    private static int PackageData_SIZE = 256;
+    //package data
+    private byte[] PackageData;
+    //Header information
+    //Header is a map from strings to strings
+    private Map<String, String> packet_header;
 
-		//package data
-		private byte[] PackageData;
+    //General Methods ---------------------------------------------------------------------
+    //Constructor for a Packet that initializes members
+    public Packet() {
+        //Initialize data array
+        PackageData = new byte[PackageData_SIZE];
 
-		//Header information
-		//Header is a map from strings to strings
-		private Map<String, String> packet_header;
+        //Initialize Map (using HashMap because it's fun)
+        packet_header = new HashMap<String, String>();
+    }
 
-		//Private constant to map segment number and checksum
-		private static final String HEADER_SEGMENT_NUM = "segmentNum";
-		private static final String HEADER_CHECKSUM = "checkSum";
+    public static Packet createPacket(DatagramPacket in) {
+        Packet newPacket = new Packet();
+        ByteBuffer bb = ByteBuffer.wrap(in.getData());
+        newPacket.setHeaderValue(HEADER_ELEMENTS.SEGMENT_NUMBER, bb.getShort() + "");
+        newPacket.setHeaderValue(HEADER_ELEMENTS.CHECKSUM, bb.getShort() + "");
 
-//General Methods ---------------------------------------------------------------------
-	//Constructor for a Packet that initializes members
-	public Packet()
-	{
-		//Initialize data array
-		PackageData = new byte[PackageData_SIZE];
+        byte[] inData = in.getData();
+        byte[] remaining = new byte[inData.length - bb.position()];
+        for (int i = 0; i < remaining.length; i++)
+            remaining[i] = inData[i + bb.position()];
+        newPacket.setPacketData(remaining);
+        return newPacket;
+    }
 
-		//Initialize Map (using HashMap because it's fun)
-		packet_header = new HashMap<String, String>();
-	}
+    //Check sum function that return the 16 bit checkSum value for a packet
+    public static short checkSum(byte[] packetBytes) {
+        short sum = 0;
+        int packetByteLength = packetBytes.length;
 
-	//Function simply displays all of the key-value pairs in the map
-	//as well as all of the bytes of data (as numerical values from
-	//0 - 255).
-	public void display()
-	{
-		System.out.println("PACKET HEADER------------------------------");
-		Iterator<Entry<String, String>> it = packet_header.entrySet().iterator();
-		while (it.hasNext())
-		{
-			Entry<String,String> n = it.next();
-			System.out.println((n.getKey()) + " : " + (n.getValue()));
-		}
-		System.out.println("PACKET DATA--------------------------------");
-		for (int i = 0; i < PackageData.length; i++)
-			System.out.print(PackageData[i] + " ");
-		System.out.println("");
-	}
+        int count = 0;
+        while (count > 1) {
+            sum += ((packetBytes[count]) << 8 & 0xFF00) | ((packetBytes[count + 1]) & 0x00FF);
+            if ((sum & 0xFFFF0000) > 0) {
+                sum = (short) ((sum & 0xFF) + 1);
+            }
+            count += 2;
+            packetByteLength -= 2;
+        }
 
-	//Increases data of the array by adding the default data size
-	//to what is already allocated.
-	private void IncreasePacketData()
-	{
-		byte[] temp = PackageData;
-		PackageData = new byte[PackageData.length + PackageData_SIZE];
-		for (int i = 0; i < temp.length; i++)
-			PackageData[i] = temp[i];
-	}
+        if (packetByteLength > 0) {
+            sum += (packetBytes[count] << 8 & 0xFF00);
+            if ((sum & 0xFFFF0000) > 0) {
+                sum = (short) ((sum & 0xFFFF) + 1);
+            }
+        }
+        return (short) (~sum & 0xFFFF);
+    }
 
-//Header Methods --------------------------------------------------------------
-	//Sets a key-value pair in the header of this packet. The keys are
-	//governed by our enumeration, HEADER_ELEMENTS, such that the
-	//packets all have the exact same headers.
-	public void setHeaderValue(HEADER_ELEMENTS ele, String value)
-	{
-		switch (ele)
-		{
-			case SEGMENT_NUMBER:
-				packet_header.put(HEADER_SEGMENT_NUM, value);
-				break;
-			case CHECKSUM:
-				packet_header.put(HEADER_CHECKSUM, value);
-				break;
-			default:
-				throw new IllegalArgumentException("Something is broken... bad broken");
-		}
-	}
+    //Function simply displays all of the key-value pairs in the map
+    //as well as all of the bytes of data (as numerical values from
+    //0 - 255).
+    public void display() {
+        System.out.println("PACKET HEADER------------------------------");
+        Iterator<Entry<String, String>> it = packet_header.entrySet().iterator();
+        while (it.hasNext()) {
+            Entry<String, String> n = it.next();
+            System.out.println((n.getKey()) + " : " + (n.getValue()));
+        }
+        System.out.println("PACKET DATA--------------------------------");
+        for (int i = 0; i < PackageData.length; i++)
+            System.out.print(PackageData[i] + " ");
+        System.out.println();
+    }
 
-	//Gets one of the values given the key governed by our
-	//enumeration, HEADER_ELEMENTS.
-	public String getHeaderValue(HEADER_ELEMENTS ele)
-	{
-		switch (ele)
-		{
-			case SEGMENT_NUMBER:
-				return packet_header.get(HEADER_SEGMENT_NUM);
-			case CHECKSUM:
-				return packet_header.get(HEADER_CHECKSUM);
-			default:
-				throw new IllegalArgumentException("HSomething is broken... bad broken");
-		}
-	}
+    //Increases data of the array by adding the default data size
+    //to what is already allocated.
+    private void IncreasePacketData() {
+        byte[] temp = PackageData;
+        PackageData = new byte[PackageData.length + PackageData_SIZE];
+        for (int i = 0; i < temp.length; i++)
+            PackageData[i] = temp[i];
+    }
+
+    //Header Methods --------------------------------------------------------------
+    //Sets a key-value pair in the header of this packet. The keys are
+    //governed by our enumeration, HEADER_ELEMENTS, such that the
+    //packets all have the exact same headers.
+    public void setHeaderValue(HEADER_ELEMENTS ele, String value) {
+        switch (ele) {
+            case SEGMENT_NUMBER:
+                packet_header.put(HEADER_SEGMENT_NUM, value);
+                break;
+            case CHECKSUM:
+                packet_header.put(HEADER_CHECKSUM, value);
+                break;
+            default:
+                throw new IllegalArgumentException("Something is broken... bad broken");
+        }
+    }
 
 //Data Methods ----------------------------------------------------------------
 
-	//Resets the data array to default size and values
-	public void resetData()
-	{
-		PackageData = new byte[PackageData_SIZE];
-	}
+    //Gets one of the values given the key governed by our
+    //enumeration, HEADER_ELEMENTS.
+    public String getHeaderValue(HEADER_ELEMENTS ele) {
+        switch (ele) {
+            case SEGMENT_NUMBER:
+                return packet_header.get(HEADER_SEGMENT_NUM);
+            case CHECKSUM:
+                return packet_header.get(HEADER_CHECKSUM);
+            default:
+                throw new IllegalArgumentException("HSomething is broken... bad broken");
+        }
+    }
 
-	//Allows user to set a singular element in the data
-	//array, given that the index is within bounds.
-	//Throws IndexOutOfBoundsException if not.
-	public void setPacketData(int index, byte val)
-	{
-		if (index >= 0)
-		{
-			while (index > PackageData.length)
-				IncreasePacketData();
-			PackageData[index] = val;
-		}
-		else
-			throw new IndexOutOfBoundsException(
-				"PACKET -- SET DATA; index = " + index);
-	}
+    //Resets the data array to default size and values
+    public void resetData() {
+        PackageData = new byte[PackageData_SIZE];
+    }
 
-	//Takes an array of bytes to be set as the data segment.
-	//If the Packet contains data already, the data is overwritten.
-	//Throws IllegalArgumentException if the size of toSet does not
-	//conform with the size of the data segment in the packet.
-	public void setPacketData(byte[] toSet) throws IllegalArgumentException
-	{
-		int argSize = toSet.length;
-		if (argSize > 0)
-		{
-			PackageData = new byte[argSize];
-			for (int i = 0; i < PackageData.length; i++)
-				PackageData[i] = toSet[i];
-		}
-		else
-			throw new IllegalArgumentException(
-				"PACKET -- SET DATA; toSet.length = " + toSet.length);
-	}
+    //Allows user to set a singular element in the data
+    //array, given that the index is within bounds.
+    //Throws IndexOutOfBoundsException if not.
+    public void setPacketData(int index, byte val) {
+        if (index >= 0) {
+            while (index > PackageData.length)
+                IncreasePacketData();
+            PackageData[index] = val;
+        } else
+            throw new IndexOutOfBoundsException(
+                    "PACKET -- SET DATA; index = " + index);
+    }
 
-	public byte getData(int index)
-	{
-		if (index >= 0 && index < PackageData.length)
-			return PackageData[index];
-		throw new IndexOutOfBoundsException(
-				"PACKET -- GET DATA; index = " + index);
-	}
+    //Takes an array of bytes to be set as the data segment.
+    //If the Packet contains data already, the data is overwritten.
+    //Throws IllegalArgumentException if the size of toSet does not
+    //conform with the size of the data segment in the packet.
+    public void setPacketData(byte[] toSet) throws IllegalArgumentException {
+        int argSize = toSet.length;
+        if (argSize > 0) {
+            PackageData = new byte[argSize];
+            for (int i = 0; i < PackageData.length; i++)
+                PackageData[i] = toSet[i];
+        } else
+            throw new IllegalArgumentException(
+                    "PACKET -- SET DATA; toSet.length = " + toSet.length);
+    }
 
-	public byte[] getData()
-	{
-		return PackageData;
-	}
+    public byte getData(int index) {
+        if (index >= 0 && index < PackageData.length)
+            return PackageData[index];
+        throw new IndexOutOfBoundsException(
+                "PACKET -- GET DATA; index = " + index);
+    }
 
-	public int getDataSize()
-	{
-		return PackageData.length;
-	}
+    public byte[] getData() {
+        return PackageData;
+    }
 
-	/**
-	 * Returns the contents of this packet as a DatagramPacket
-	 * @param i the IP Address to give to the DatagramPacket
-	 * @param port the port number to give to the DatagramPacket
-	 * @return returns the DatagramPacket fully ready to be sent
-	 */
-	public DatagramPacket getDatagramPacket(InetAddress i, int port)
-	{
-		byte[] setData = ByteBuffer.allocate(256)
-				.putShort(Short.parseShort(packet_header.get(HEADER_SEGMENT_NUM)))
-				.putShort(Short.parseShort(packet_header.get(HEADER_CHECKSUM)))
-				.put(PackageData)
-				.array();
+    public int getDataSize() {
+        return PackageData.length;
+    }
 
-		return new DatagramPacket(setData, setData.length, i, port);
-	}
+    /**
+     * Returns the contents of this packet as a DatagramPacket
+     *
+     * @param i    the IP Address to give to the DatagramPacket
+     * @param port the port number to give to the DatagramPacket
+     * @return returns the DatagramPacket fully ready to be sent
+     */
+    public DatagramPacket getDatagramPacket(InetAddress i, int port) {
+        byte[] setData = ByteBuffer.allocate(256)
+                .putShort(Short.parseShort(packet_header.get(HEADER_SEGMENT_NUM)))
+                .putShort(Short.parseShort(packet_header.get(HEADER_CHECKSUM)))
+                .put(PackageData)
+                .array();
 
-	public static Packet createPacket(DatagramPacket in)
-	{
-		Packet newPacket = new Packet();
-		ByteBuffer bb = ByteBuffer.wrap(in.getData());
-		newPacket.setHeaderValue(HEADER_ELEMENTS.SEGMENT_NUMBER, bb.getShort()+"");
-		newPacket.setHeaderValue(HEADER_ELEMENTS.CHECKSUM, bb.getShort()+"");
+        return new DatagramPacket(setData, setData.length, i, port);
+    }
 
-		byte[] inData = in.getData();
-		byte[] remaining = new byte[inData.length - bb.position()];
-		for (int i = 0; i < remaining.length; i++)
-			remaining[i] = inData[i+bb.position()];
-		newPacket.setPacketData(remaining);
-		return newPacket;
-	}
-	  //Check sum function that return the 16 bit checkSum value for a packet
-	  public static short checkSum(byte[] packetBytes) {
-		  short sum = 0;
-		  int packetByteLength = packetBytes.length;
-
-		  int count = 0;
-		  while (count > 1) {
-			  sum += ((packetBytes[count]) << 8 & 0xFF00) | ((packetBytes[count + 1]) & 0x00FF);
-			  if ((sum & 0xFFFF0000) > 0) {
-				  sum = (short) ((sum & 0xFF) + 1);
-			  }
-			  count += 2;
-			  packetByteLength -=2;
-		  }
-
-		  if(packetByteLength > 0) {
-			  sum += (packetBytes[count] << 8 & 0xFF00);
-			  if ((sum & 0xFFFF0000) > 0) {
-				  sum = (short) ((sum & 0xFFFF) + 1);
-			  }
-		  }
-		  return (short) (~sum & 0xFFFF);
-	  }
+    public enum HEADER_ELEMENTS {
+        SEGMENT_NUMBER,
+        CHECKSUM
+    }
 
 }
--- Lab 1/UDPClient.java
+++ Lab 1/UDPClient.java
@@ -1,114 +1,102 @@
-import java.io.*;
-import java.net.*;
-import java.util.*;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.util.ArrayList;
+import java.util.Random;
 
 public class UDPClient {
 
-	public static void main(String args[]) throws Exception {
-		int[] ports = {10028, 10029, 10030, 10031}; //Group Assigned Port Numbers
-		int port = ports[0];  //must be the same as port in server file
-	  //use tux050 tux065
+    public static void main(String args[]) throws Exception {
+        int[] ports = {10028, 10029, 10030, 10031}; //Group Assigned Port Numbers
+        int port = ports[0];  //must be the same as port in server file
+        //use tux050 tux065
 
+        DatagramSocket clientSocket = new DatagramSocket();        //creates socket for user
 
-		BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in)); 	//sets up input reader
-		DatagramSocket clientSocket = new DatagramSocket();		//creates socket for user
-		String localhost = InetAddress.getLocalHost().getHostAddress().trim();	//gets user IP
-		InetAddress IPAddress = InetAddress.getByName("172.19.145.232");	//gets IP address of host
+        InetAddress IPAddress = InetAddress.getByName("172.19.145.232");    //gets IP address of host
 
-		byte[] sendData = new byte[256];	//creates packet to be sent
-		byte[] receiveData = new byte[256]; //cretes packet to be received
+        byte[] sendData;    //creates packet to be sent
+        byte[] receiveData = new byte[256]; //creates packet to be received
 
-		String TestFile = "GET TestFile.html HTTP/1.0";
-		sendData = TestFile.getBytes();
+        String TestFile = "GET TestFile.html HTTP/1.0";
+        sendData = TestFile.getBytes();
 
-		DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, port);
-		//sending the packet
-		clientSocket.send(sendPacket);
-		//notify user of sending
-		System.out.println("Sending request packet....");
+        DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, port);
+        //sending the packet
+        clientSocket.send(sendPacket);
+        //notify user of sending
+        System.out.println("Sending request packet....");
 
-		DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+        DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
 
-		clientSocket.receive(receivePacket);
+        clientSocket.receive(receivePacket);
 
-		String modifiedTestFile = new String(receivePacket.getData());
+        String modifiedTestFile = new String(receivePacket.getData());
 
-		System.out.println("FROM SERVER" + modifiedTestFile);
-		clientSocket.close();
+        System.out.println("FROM SERVER" + modifiedTestFile);
+        clientSocket.close();
 
-	}
+    }
 
-	 private static void Gremlin(String probOfDamage, Packet receivedPacket)
-	    {
-	    	Random random = new Random();
-	    	//pick a random number
-	    	int dmgRand = random.nextInt(100) + 1;
-				int howManyRand = random.nextInt(100) + 1;
-				int bytesToChange;
-				if (howManyRand <= 50) {
-					bytesToChange = 1;
-				} else if (50 < howManyRand && howManyRand <= 80) {
-					bytesToChange = 2;
-				} else bytesToChange = 3;
-	    	//pick a
-	    	double damagedProbability = Double.parseDouble(probOfDamage) * 100;
-	    	if (dmgRand <= damagedProbability)
-	    	{
-					for (int i = 0; i <= bytesToChange; i++){
-	    			byte[] data = receivedPacket.getData();
-	    			int byteToCorrupt = random.nextInt(receivedPacket.getDataSize()); // pick a random byte
-	    			data[byteToCorrupt] = (byte) (~data[byteToCorrupt] | 0xFF); // flip the bits in that byte
-	    	}
+    private static void Gremlin(String probOfDamage, Packet receivedPacket) {
+        Random random = new Random();
+        //pick a random number
+        int dmgRand = random.nextInt(100) + 1;
+        int howManyRand = random.nextInt(100) + 1;
+        int bytesToChange;
+        if (howManyRand <= 50) {
+            bytesToChange = 1;
+        } else if (howManyRand <= 80) {
+            bytesToChange = 2;
+        } else bytesToChange = 3;
+        //pick a
+        double damagedProbability = Double.parseDouble(probOfDamage) * 100;
+        if (dmgRand <= damagedProbability) {
+            for (int i = 0; i <= bytesToChange; i++) {
+                byte[] data = receivedPacket.getData();
+                int byteToCorrupt = random.nextInt(receivedPacket.getDataSize()); // pick a random byte
+                data[byteToCorrupt] = (byte) (~data[byteToCorrupt] | 0xFF); // flip the bits in that byte
+            }
 
-		}
-	}
+        }
+    }
 
-	    private static boolean ErrorDetection(ArrayList<Packet> PacketList)
-	    {
-	    	for (int i = 0; i < PacketList.size(); i++)
-	    	{
-	    		String strReceivedCheckSum = PacketList.get(i).getHeaderValue(Packet.HEADER_ELEMENTS.CHECKSUM);
-	    		Short receivedCheckSum = Short.parseShort(strReceivedCheckSum);
+    private static boolean ErrorDetection(ArrayList<Packet> PacketList) {
+        for (Packet packet : PacketList) {
+            String strReceivedCheckSum = packet.getHeaderValue(Packet.HEADER_ELEMENTS.CHECKSUM);
+            Short receivedCheckSum = Short.parseShort(strReceivedCheckSum);
 
-	    		byte[] data = PacketList.get(i).getData();
-	    		int calcCheckSum = Packet.checkSum(data);
-	    		System.out.println("Post-Gremlin checksum: " + String.valueOf(calcCheckSum));
-	    		if (!receivedCheckSum.equals(calcCheckSum))
-	    			return true;
-	    	}
+            byte[] data = packet.getData();
+            int calcCheckSum = Packet.checkSum(data);
+            System.out.println("Post-Gremlin checksum: " + String.valueOf(calcCheckSum));
+            if (!receivedCheckSum.equals(calcCheckSum))
+                return true;
+        }
 
-	    	return false;
-	    }
+        return false;
+    }
 
+    private static byte[] ReassemblePacket(ArrayList<Packet> PacketList) {
+        int totalSize = 0;
+        for (Packet packet : PacketList) totalSize += packet.getDataSize();
 
+        byte[] returnPacket = new byte[totalSize];
+        int retCounter = 0;
+        for (int i = 0; i < PacketList.size(); i++) {
+            //Do a boring linear search on the list for each packet number
+            for (Packet check : PacketList) {
+                String segNum = check.getHeaderValue(
+                        Packet.HEADER_ELEMENTS.SEGMENT_NUMBER);
+                if (Integer.parseInt(segNum) == i) {
+                    for (int k = 0; k < check.getDataSize(); k++)
+                        returnPacket[retCounter + k] = check.getData(k);
+                    retCounter += check.getDataSize();
+                    break;
+                }
+            }
+        }
 
-		private static byte[] ReassemblePacket(ArrayList<Packet> PacketList)
-	    {
-	    	int totalSize = 0;
-	    	for (int i = 0; i < PacketList.size(); i++)
-	    		totalSize += PacketList.get(i).getDataSize();
-
-	    	byte[] returnPacket = new byte[totalSize];
-	    	int retCounter = 0;
-	    	for (int i = 0; i < PacketList.size(); i++)
-	    	{
-		    	//Do a boring linear search on the list for each packet number
-		    	for (int j = 0; j < PacketList.size(); j++)
-		    	{
-		    		Packet check = PacketList.get(j);
-		    		String segNum = check.getHeaderValue(
-										Packet.HEADER_ELEMENTS.SEGMENT_NUMBER);
-		    		if (Integer.parseInt(segNum) == i)
-		    		{
-		    			for (int k = 0; k < check.getDataSize(); k++)
-		    				returnPacket[retCounter + k] = check.getData(k);
-		    			retCounter += check.getDataSize();
-		    			break;
-		    		}
-		    	}
-	    	}
-
-	    	return returnPacket;
-	    }
+        return returnPacket;
+    }
 
 }
